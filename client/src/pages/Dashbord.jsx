8bcbb7867047b7251e04e2218b35739122745300
Poorvik P <poorvikp508@gmail.com>
2025-10-13 20:36:14 +0530

    navbar done

--- client/src/pages/Dashbord.jsx
+++ client/src/pages/Dashbord.jsx
@@ -1,11 +1,22 @@
+import { PlusIcon } from "lucide-react"
 import React from 'react'
 
 const Dashbord = () => {
   return (
     <div>
-          <h1>
- Dashbord
-      </h1>
+      <div className="max-7xl mx-auto px-4 py-8">
+        <p>Welcome,Joe Doe</p>
+        <div className="flex gap-4 ">
+          <button className="w-full">
+            <PlusIcon className="size-11 transition-all duration-300 p-2.5 bg-gradient-to-br from-indigo-300 to-indigo-500 text-white rounded-full"/>
+            <p>Create Resume</p>
+          </button>
+          <button></button>
+          
+        </div>
+        
+      </div>
+      
     </div>
   )
 }
