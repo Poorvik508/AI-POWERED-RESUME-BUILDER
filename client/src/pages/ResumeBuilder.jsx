ded9f46874556467c973b9abc50d7691f3b18373
Poorvik P <poorvikp508@gmail.com>
2025-10-16 19:05:02 +0530

    colorpicker component done

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -4,6 +4,8 @@ import { dummyResumeData } from "../assets/assets";
 import { ArrowLeftIcon, Briefcase, ChevronLeft, ChevronRight, FileText, Flame, GraduationCap, Sparkles, User } from "lucide-react";
 import PersonalInfoForm from "../components/PersonalInfoForm";
 import ResumePreview from "../components/ResumePreview";
+import TemplateSelector from "../components/TemplateSelector";
+import ColorPicker from "../components/ColorPicker";
 
 const ResumeBuilder = () => {
   const { resumeId } = useParams();
@@ -70,10 +72,12 @@ const ResumeBuilder = () => {
               />
 
               {/* section navigation */}
-              <div className="flex justify-between items-center mb-6 border-b border-gray-300 py-1">
-                <div>
-                  
+              <div className="flex  items-center mb-6 border-b border-gray-300 py-1">
+                <div className="flex  items-center gap-2">
+                  <TemplateSelector selectedTemplate={resumeData.template} onChange={(template)=>setResumeData(prev=>({...prev,template}))}/>
+                    <ColorPicker selectedColor={resumeData.accent_color} onChange={(color)=>setResumeData(prev=>({...prev,accent_color:color}))}/>
                 </div>
+
                 <div className="flex items-center">
                   {activeSectionIndex !== 0 && (
                     <button
